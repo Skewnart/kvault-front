22754d3807c6a113635d352d7addb3c92795877b
skewnart <corentinz@outlook.fr>
2026-07-26 19:46:24 +0200

    docs: adding todos

--- src/routes/folder/+page.svelte
+++ src/routes/folder/+page.svelte
@@ -11,8 +11,9 @@
 	const TITLE = "Kvault";
 
 	// TODO : Refaire le design sur toutes les pages APRES être passé sur toutes les pages pour les FEATURES qui ne changeront pas avec le design
-
-	// TODO Rediriger vers le logo quand le token est dépassé
+	// TODO : Rediriger vers le logo quand le token est dépassé
+	// TODO : Faire la recherche sur les deux pages concernées
+	// TODO : Faire une flèche pour revenir à la page précédente
 
 	const props = $props();
 	let error = $state("");
